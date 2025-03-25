4140cf853d85ddc103a481f853822c82a37ac38c
Vadim Popovych <zyzel19@gmail.com>
2025-03-25 17:17:57 +0100

    feat(GameObject): set default position - (0,0)

--- src/main/java/com/game/core/behaviour/base/GameObject.java
+++ src/main/java/com/game/core/behaviour/base/GameObject.java
@@ -3,17 +3,20 @@ package com.game.core.behaviour.base;
 import com.game.core.behaviour.interfaces.Positionable;
 
 public abstract class GameObject implements Positionable {
-    private float x;
-    private float y;
+    private float x = 0;
+    private float y = 0;
 
+    @Override
     public float getX() {
         return this.x;
     }
 
+    @Override
     public float getY() {
         return this.y;
     }
 
+    @Override
     public void setPos(float x, float y) {
         this.x = x;
         this.y = y;
