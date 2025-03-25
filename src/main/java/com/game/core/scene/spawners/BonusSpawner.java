1533d7fb4a3689759cf2e028e2c7329b5825586b
Vadim Popovych <zyzel19@gmail.com>
2025-03-25 19:35:31 +0100

    feat(BonusSpawner): use Timer for periodical spawn

--- src/main/java/com/game/core/scene/spawners/BonusSpawner.java
+++ src/main/java/com/game/core/scene/spawners/BonusSpawner.java
@@ -2,17 +2,20 @@ package com.game.core.scene.spawners;
 
 import com.game.core.behaviour.base.GameObject;
 import com.game.core.scene.graphics.Tile;
+import com.game.core.utils.Timer;
 import javafx.scene.canvas.GraphicsContext;
 
 import java.util.Objects;
 
 public class BonusSpawner extends GameObject implements Spawner {
     private float cooldown;
-    private float timeToNext;
+    private Timer<BonusSpawner> spawnTimer;
     private Tile tile;
 
-    public BonusSpawner(Tile tile) {
+    public BonusSpawner(Tile tile, float cooldown) {
         setTile(tile);
+        setCooldown(cooldown);
+        createSpawnTimer();
     }
 
     @Override
@@ -27,23 +30,28 @@ public class BonusSpawner extends GameObject implements Spawner {
 
     @Override
     public void update(double deltaTime) {
+        getSpawnTimer().decreaseTime(deltaTime);
 
+        if (getSpawnTimer().getTimeLeft() <= 0) {
+            getSpawnTimer().getFunc().accept(this);
+            getSpawnTimer().setTimeLeft(getSpawnTimer().getBaseTime());
+        }
+    }
+
+    public Timer<BonusSpawner> getSpawnTimer() {
+        return this.spawnTimer;
+    }
+
+    private void createSpawnTimer() {
+        this.spawnTimer = new Timer<>(getCooldown(), BonusSpawner::spawn, true);
     }
 
     public float getCooldown() {
         return cooldown;
     }
 
-    public void setCooldown(float cooldown) {
+    private void setCooldown(float cooldown) {
         this.cooldown = cooldown;
-    }
-
-    public float getTimeToNext() {
-        return timeToNext;
-    }
-
-    public void setTimeToNext(float timeToNext) {
-        this.timeToNext = timeToNext;
     }
 
     @Override
