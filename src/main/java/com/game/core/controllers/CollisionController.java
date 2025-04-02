ebbd59c376d393471a9f9824b85047bfb9811003
Vadim Popovych <zyzel19@gmail.com>
2025-04-02 18:36:40 +0200

    feat(collisions): add collision controller

--- src/main/java/com/game/core/controllers/CollisionController.java
+++ src/main/java/com/game/core/controllers/CollisionController.java
@@ -1,4 +1,26 @@
 package com.game.core.controllers;
 
+import com.game.core.behaviour.base.CollidableGameObject;
+import com.game.core.scene.Scene;
+
+import java.util.List;
+
 public class CollisionController {
+    private final CollisionVisitor collisionHandler = new CollisionHandler();
+
+    public void checkCollisions(Scene scene) {
+        List<CollidableGameObject> objs = scene.getCollidableGameObjects();
+
+        for (int i = 0; i < objs.size(); i++) {
+            CollidableGameObject obj1 = objs.get(i);
+
+            for (int j = i + 1; j < objs.size(); j++) {
+                CollidableGameObject obj2 = objs.get(j);
+
+                if(obj1.intersects(obj2)) {
+                    objs.get(i).onCollision(collisionHandler, objs.get(j));
+                }
+            }
+        }
+    }
 }
