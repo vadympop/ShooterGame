72185affbc0ffbc363267b327965686fa2c286e6
Vadim Popovych <zyzel19@gmail.com>
2025-04-03 18:17:21 +0200

    feat: add tiles and pass tile to Bonus in BonusFactory

--- src/main/java/com/game/core/entities/bonus/BonusFactory.java
+++ src/main/java/com/game/core/entities/bonus/BonusFactory.java
@@ -1,16 +1,18 @@
 package com.game.core.entities.bonus;
 
 import com.game.core.effects.*;
+import com.game.core.scene.graphics.Tile;
 
 public class BonusFactory {
     public static Bonus createBonus(BonusType type) {
+        Tile tile = new Tile("bonus.png", 3);
         return switch (type) {
-            case SHIELD -> new Bonus(new ShieldEffect());
-            case SPEED_BOOST -> new Bonus(new SpeedBoostEffect());
-            case DOUBLE_SHOOT -> new Bonus(new DoubleShootEffect());
-            case DOUBLE_DAMAGE -> new Bonus(new DoubleDamageEffect());
-            case CIRCULAR_SHOOT -> new Bonus(new CircularShootEffect());
-            default -> new Bonus(new NoEffect());
+            case SHIELD -> new Bonus(new ShieldEffect(), tile);
+            case SPEED_BOOST -> new Bonus(new SpeedBoostEffect(), tile);
+            case DOUBLE_SHOOT -> new Bonus(new DoubleShootEffect(), tile);
+            case DOUBLE_DAMAGE -> new Bonus(new DoubleDamageEffect(), tile);
+            case CIRCULAR_SHOOT -> new Bonus(new CircularShootEffect(), tile);
+            default -> new Bonus(new NoEffect(), tile);
         };
     }
 }
