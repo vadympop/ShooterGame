33e9ef86861920fcba4a7f93b8410baae1218934
Vadim Popovych <zyzel19@gmail.com>
2025-04-04 09:42:18 +0200

    feat(SceneLoader): remove configs from this class, add generatePos, loadAreas and loadSpawners methods, and add config field into class

--- src/main/java/com/game/core/utils/SceneLoader.java
+++ src/main/java/com/game/core/utils/SceneLoader.java
@@ -1,55 +1,90 @@
 package com.game.core.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.game.core.scene.Scene;
+import com.game.core.behaviour.bounds.RectangleBounds;
+import com.game.core.scene.spawners.BonusSpawner;
+import com.game.core.scene.spawners.PlayerSpawner;
+import com.game.core.scene.spawners.Spawner;
+import com.game.core.utils.config.SceneConfig;
+import com.game.gui.views.game.GameScene;
 import com.game.core.scene.blocks.SolidBlock;
 import com.game.core.scene.graphics.SceneTile;
 import com.game.core.scene.graphics.Tile;
 import com.game.core.scene.graphics.TileType;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class SceneLoader {
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final String pathToScenes = "scenes/";
+    private SceneConfig config;
 
-    public Scene loadScene(String sceneId) throws IOException {
-        File file = new File(this.pathToScenes + sceneId + "_scene.json");
-        SceneConfig config = mapper.readValue(file, SceneConfig.class);
+    public SceneLoader(SceneConfig config) {
+        setConfig(config);
+    }
 
-        Scene newScene = new Scene(sceneId, config.getName());
-        loadTiles(newScene, config, config.getBackgroundTiles(), TileType.BACKGROUND);
-        loadTiles(newScene, config, config.getOverlayTiles(), TileType.OVERLAY);
-        loadBlocks(newScene, config, config.getBlocks());
+    public GameScene loadScene() {
+        GameScene newScene = new GameScene(getConfig().getId(), config.getName());
+        loadTiles(newScene, config.getBackgroundTiles(), TileType.BACKGROUND);
+        loadTiles(newScene, config.getOverlayTiles(), TileType.OVERLAY);
+        loadBlocks(newScene, config.getBlocks());
+        loadSpawners(newScene);
+        loadAreas(newScene);
 
         return newScene;
     }
 
-    public void loadTiles(Scene scene, SceneConfig config, List<String> tiles, TileType tileType) {
-        loadObjectsFromStrings(scene, config, tiles, tileType, (s, t, x, y) -> {
+    public float[] generatePos(int colsCount, int rowsCount) {
+        SceneConfig config = getConfig();
+
+        float xPos = colsCount * config.getTileWidth() * config.getScale() - (config.getTileWidth() / 2);
+        float yPos = rowsCount * config.getTileHeight() * config.getScale() - (config.getTileHeight() / 2);
+        return new float[]{xPos, yPos};
+    }
+
+    public void loadAreas(GameScene scene) {
+        getConfig().getAreas().forEach(x -> {
+
+        });
+    }
+
+    public void loadSpawners(GameScene scene) {
+        getConfig().getSpawners().forEach(x -> {
+            Spawner spawner;
+            if (x.getType() == 0) {
+                spawner = new PlayerSpawner(new Tile(x.getTexture(), getConfig().getScale()), new Tile(x.getPlayerTexture(), getConfig().getScale()));
+                spawner.addEvent("onPlayerCreated", scene::addEntity);
+            }
+            else if (x.getType() == 1) spawner = new BonusSpawner(new Tile(x.getTexture(), getConfig().getScale()), x.getCooldown());
+            else return;
+
+            float[] pos = generatePos(x.getCol(), x.getRow());
+            spawner.setPos(pos[0], pos[1]);
+            scene.addSpawner(spawner);
+        });
+    }
+
+    public void loadTiles(GameScene scene, List<String> tiles, TileType tileType) {
+        loadObjectsFromStrings(scene, getConfig().getMappings().getTiles(), tiles, tileType, (s, t, x, y) -> {
             SceneTile sceneTile = new SceneTile(t);
             sceneTile.setPos(x, y);
             s.addTile(sceneTile);
         });
     }
 
-    public void loadBlocks(Scene scene, SceneConfig config, List<String> blocks) {
-        loadObjectsFromStrings(scene, config, blocks, null, (s, t, x, y) -> {
+    public void loadBlocks(GameScene scene, List<String> blocks) {
+        loadObjectsFromStrings(scene, getConfig().getMappings().getBlocks(), blocks, null, (s, t, x, y) -> {
             SolidBlock block = new SolidBlock(t);
             block.setPos(x, y);
-            s.addCollidableGameObject(block);
+            block.setBounds(new RectangleBounds(32, 32));
+            s.addBlock(block);
         });
     }
 
     public void loadObjectsFromStrings(
-            Scene scene,
-            SceneConfig config,
+            GameScene scene,
+            Map<String, String> texturesMapping,
             List<String> elements,
             TileType tileType,
-            QuatroConsumer<Scene, Tile, Float, Float> addFunction
+            QuatroConsumer<GameScene, Tile, Float, Float> addFunction
     ) {
         int rowsCount = 0;
         for (String row : elements) {
@@ -60,137 +95,16 @@ public class SceneLoader {
                 if (x == '-') continue;
                 String elementStr = String.valueOf(x);
                 Tile tile = new Tile(
-                        config.getMappings().getTiles().get(elementStr),
+                        texturesMapping.get(elementStr),
                         tileType,
-                        config.getScale()
+                        getConfig().getScale()
                 );
-                float xPos = colsCount * config.getTileWidth();
-                float yPos = rowsCount * config.getTileHeight();
-                addFunction.accept(scene, tile, xPos, yPos);
+                float[] pos = generatePos(colsCount, rowsCount);
+                addFunction.accept(scene, tile, pos[0], pos[1]);
             }
         }
     }
 
-    public static class SceneConfig {
-        private String id;
-        private float scale;
-        private float tileWidth;
-        private float tileHeight;
-        private String name;
-        private List<Area> areas;
-        private List<Spawner> spawners;
-        private List<String> backgroundTiles;
-        private List<String> blocks;
-        private List<String> overlayTiles;
-        private Mappings mappings;
-        private Player player;
-
-        public String getId() { return id; }
-        public void setId(String id) { this.id = id; }
-
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-
-        public List<Area> getAreas() { return areas; }
-        public void setAreas(List<Area> areas) { this.areas = areas; }
-
-        public List<Spawner> getSpawners() { return spawners; }
-        public void setSpawners(List<Spawner> spawners) { this.spawners = spawners; }
-
-        public List<String> getBackgroundTiles() { return backgroundTiles; }
-        public void setBackgroundTiles(List<String> backgroundTiles) { this.backgroundTiles = backgroundTiles; }
-
-        public List<String> getBlocks() { return blocks; }
-        public void setBlocks(List<String> blocks) { this.blocks = blocks; }
-
-        public List<String> getOverlayTiles() { return overlayTiles; }
-        public void setOverlayTiles(List<String> overlayTiles) { this.overlayTiles = overlayTiles; }
-
-        public Mappings getMappings() { return mappings; }
-        public void setMappings(Mappings mappings) { this.mappings = mappings; }
-
-        public float getScale() { return scale; }
-        public void setScale(float scale) { this.scale = scale; }
-
-        public float getTileWidth() { return tileWidth; }
-        public void setTileWidth(float tileWidth) { this.tileWidth = tileWidth; }
-
-        public float getTileHeight() { return tileHeight; }
-        public void setTileHeight(float tileHeight) { this.tileHeight = tileHeight; }
-    }
-
-    public static class Player {
-        private String texture;
-
-        public String getTexture() { return texture; }
-        public void setTexture(String texture) { this.texture = texture; }
-    }
-
-    public static class Area {
-        private String type;
-        private String col;
-        private String row;
-        private Bounds bounds;
-
-        public String getType() { return type; }
-        public void setType(String type) { this.type = type; }
-
-        public String getCol() { return col; }
-        public void setCol(String col) { this.col = col; }
-
-        public String getRow() { return row; }
-        public void setRow(String row) { this.row = row; }
-
-        public Bounds getBounds() { return bounds; }
-        public void setBounds(Bounds bounds) { this.bounds = bounds; }
-    }
-
-    public static class Bounds {
-        private String type;
-        private String width;
-        private String height;
-
-        public String getType() { return type; }
-        public void setType(String type) { this.type = type; }
-
-        public String getWidth() { return width; }
-        public void setWidth(String width) { this.width = width; }
-
-        public String getHeight() { return height; }
-        public void setHeight(String height) { this.height = height; }
-    }
-
-    public static class Spawner {
-        private String type;
-        private String col;
-        private String row;
-        private String cooldown;
-
-        public String getType() { return type; }
-        public void setType(String type) { this.type = type; }
-
-        public String getCol() { return col; }
-        public void setCol(String col) { this.col = col; }
-
-        public String getRow() { return row; }
-        public void setRow(String row) { this.row = row; }
-
-        public String getCooldown() { return cooldown; }
-        public void setCooldown(String cooldown) { this.cooldown = cooldown; }
-    }
-
-    public static class Mappings {
-        private Map<String, String> tiles;
-        private Map<String, String> spawners;
-        private Map<String, String> blocks;
-
-        public Map<String, String> getTiles() { return tiles; }
-        public void setTiles(Map<String, String> tiles) { this.tiles = tiles; }
-
-        public Map<String, String> getSpawners() { return spawners; }
-        public void setSpawners(Map<String, String> spawners) { this.spawners = spawners; }
-
-        public Map<String, String> getBlocks() { return blocks; }
-        public void setBlocks(Map<String, String> blocks) { this.blocks = blocks; }
-    }
+    public SceneConfig getConfig() { return config; }
+    private void setConfig(SceneConfig config) { this.config = config; }
 }
