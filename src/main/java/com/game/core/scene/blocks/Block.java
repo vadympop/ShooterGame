d1fd7a67f9dcf01b256493b279bbc2d2d01b3e7a
Vadim Popovych <zyzel19@gmail.com>
2025-04-04 09:33:58 +0200

    feat(Block): Block interface also extends from Collidable

--- src/main/java/com/game/core/scene/blocks/Block.java
+++ src/main/java/com/game/core/scene/blocks/Block.java
@@ -1,5 +1,6 @@
 package com.game.core.scene.blocks;
 
+import com.game.core.behaviour.interfaces.Collidable;
 import com.game.core.behaviour.interfaces.Renderable;
 
-public interface Block extends Renderable {}
+public interface Block extends Collidable, Renderable {}
