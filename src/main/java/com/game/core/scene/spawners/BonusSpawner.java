bdd6b6f5d5c7d9e54b081cdd1662c9fcd3eca657
Vadim Popovych <zyzel19@gmail.com>
2025-04-04 09:38:44 +0200

    feat(spawners): add event system and use GameObjectsFactory in PlayerSpawner

--- src/main/java/com/game/core/scene/spawners/BonusSpawner.java
+++ src/main/java/com/game/core/scene/spawners/BonusSpawner.java
@@ -1,14 +1,19 @@
 package com.game.core.scene.spawners;
 
 import com.game.core.behaviour.base.GameObject;
+import com.game.core.entities.Entity;
 import com.game.core.scene.graphics.Tile;
 import com.game.core.utils.Timer;
 import javafx.scene.canvas.GraphicsContext;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class BonusSpawner extends GameObject implements Spawner {
     private float cooldown;
+    private final Map<String, Consumer<Entity>> events = new HashMap<>();
     private Timer<BonusSpawner> spawnTimer;
     private Tile tile;
 
@@ -21,6 +26,16 @@ public class BonusSpawner extends GameObject implements Spawner {
     @Override
     public void spawn() {
 
+    }
+
+    @Override
+    public void addEvent(String name, Consumer<Entity> event) {
+        this.events.put(name, event);
+    }
+
+    @Override
+    public Consumer<Entity> getEvent(String name) {
+        return this.events.get(name);
     }
 
     @Override
--- src/main/java/com/game/core/scene/spawners/PlayerSpawner.java
+++ src/main/java/com/game/core/scene/spawners/PlayerSpawner.java
@@ -1,25 +1,48 @@
 package com.game.core.scene.spawners;
 
 import com.game.core.behaviour.base.GameObject;
+import com.game.core.entities.Entity;
 import com.game.core.entities.Player;
 import com.game.core.scene.graphics.Tile;
+import com.game.core.utils.GameObjectsFactory;
 import javafx.scene.canvas.GraphicsContext;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class PlayerSpawner extends GameObject implements Spawner {
     private Tile tile;
+    private Tile playerTile;
+    private final Map<String, Consumer<Entity>> events = new HashMap<>();
     private Player player = null;
 
-    public PlayerSpawner(Tile tile) {
+    public PlayerSpawner(Tile tile, Tile playerTile) {
         setTile(tile);
+        setPlayerTile(playerTile);
     }
 
     @Override
     public void spawn() {
-        if (getPlayer() == null) setPlayer(new Player(this, new Tile(), new Tile()));
+        if (getPlayer() == null) {
+            setPlayer(GameObjectsFactory.createPlayer(this));
+
+            Consumer<Entity> event = getEvent("onPlayerCreated");
+            if (event != null) event.accept(getPlayer());
+        }
 
         getPlayer().respawn(getX(), getY());
+    }
+
+    @Override
+    public void addEvent(String name, Consumer<Entity> event) {
+        this.events.put(name, event);
+    }
+
+    @Override
+    public Consumer<Entity> getEvent(String name) {
+        return this.events.get(name);
     }
 
     @Override
@@ -37,4 +60,7 @@ public class PlayerSpawner extends GameObject implements Spawner {
 
     public Player getPlayer() { return player; }
     public void setPlayer(Player player) { this.player = player; }
+
+    public Tile getPlayerTile() { return playerTile; }
+    public void setPlayerTile(Tile playerTile) { this.playerTile = playerTile; }
 }
