65d6a0578d06976f59c610fd7a535f329445c6d6
Vadim Popovych <zyzel19@gmail.com>
2025-04-18 16:40:54 +0200

    feat: add InvalidParameterException

---
+++ src/main/java/com/game/core/exceptions/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package com.game.core.exceptions;
+
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
