fd81e990a6d483e5482759c555b7a9dfd44097d6
Vadim Popovych <zyzel19@gmail.com>
2025-04-24 23:47:25 +0200

    feat(gui): add RenderUtils

---
+++ src/main/java/com/game/gui/utils/RenderUtils.java
@@ -0,0 +1,35 @@
+package com.game.gui.utils;
+
+import javafx.geometry.VPos;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+
+public final class RenderUtils {
+    private RenderUtils() {}
+
+    public static void drawTextInOval(
+            GraphicsContext gc,
+            double x,
+            double y,
+            double width,
+            double height,
+            String text
+    ) {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(x, y, width, height);
+
+        gc.setStroke(Color.WHITE);
+        gc.setLineWidth(2);
+        gc.strokeOval(x, y, width, height);
+
+        gc.setFill(Color.WHITE);
+        gc.setFont(new Font("Arial", 18));
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+        double centerX = x + width / 2.0;
+        double centerY = y + height / 2.0;
+        gc.fillText(text, centerX, centerY);
+    }
+}
